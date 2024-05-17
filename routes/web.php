6cc6cbf9acb2f983831fa4a0b8ae65c137d049e2
KaFatha <fathatursina@gmail.com>
2024-05-17 16:20:16 +0700

    upload tugas

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AboutController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/beranda', function () {
+    return "Halaman Beranda";
+});
+
+Route::get('/salam', function () {
+    return "Selamat Datang Fatha di Laravel 11";
+});
+
+Route::get('/profile', function () {
+    return "Halamn Profile";
+});
+
+
+//Praktikum Laravel 2
+Route::get('/dashboard', [AdminController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
